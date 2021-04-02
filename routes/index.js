9712a775981ad4e94efd92ce8e6c32ed24609ccf
Sergio Figueroa <figueroa.sergio.26@gmail.com>
2021-04-01 19:30:27 -0500

    cration of cookies for authentication

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-router.get('/login', function(req, res, next) {
-  res.render('login');
-});
-
+router.get("/logout",(req,res)=>{
+res.clearCookie("user")
+res.redirect("/login")
+})
 module.exports = router;
